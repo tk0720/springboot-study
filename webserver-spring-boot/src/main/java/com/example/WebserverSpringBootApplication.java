17c992d9609fa9fbbf8cc25d7559723b41e89b51
tk0720 <tk0720@naver.com>
2020-08-04 19:39:57 +0900

    내장 웹서버 https http2

---
+++ webserver-spring-boot/src/main/java/com/example/WebserverSpringBootApplication.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebserverSpringBootApplication {
+	
+	@GetMapping("hello")
+	public String hello() {
+		return "hello spring";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverSpringBootApplication.class, args);
+	}
+
+	/* 멀티 커넥터 설정
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+	
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		return connector;
+	}
+	*/
+
+}
